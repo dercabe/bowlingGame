9dcac097b64cd86ef9cdf3e709897a1bc1c7dee5
Clob <34946510+dercabe@users.noreply.github.com>
2018-01-01 16:52:45 -0800

    Adding main file

---
+++ New1.js.js
@@ -0,0 +1,8 @@
+const firstDie = [1, 2, 3, 4, ,5 ,6];
+const secondDie = firstDie
+
+let rollDice = () => {
+	firstDieValue = firstDie.Math(random);
+	secondDieValue = secondDie.Math.(random);
+	Return values[firstDieValue, secondDieValue];
+}
